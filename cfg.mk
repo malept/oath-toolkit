f289f623d996b25e385f819176c648e3fceecccb
Simon Josefsson <simon@josefsson.org>
2009-12-07 22:17:01 +0100

    Fix make distcheck.

--- cfg.mk
+++ cfg.mk
@@ -18,7 +18,6 @@ local-checks-to-skip = sc_GPL_version sc_immutable_NEWS sc_prohibit_strcmp
 # Re-add when we have translation
 local-checks-to-skip += sc_unmarked_diagnostics
 
-.PHONY: ChangeLog
 ChangeLog:
 	git2cl > ChangeLog
 	cat .clcopying >> ChangeLog
