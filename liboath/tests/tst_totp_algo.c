c12bed5ea1c31bea4ea2fcbb82f9c06561443af3
Simon Josefsson <simon@josefsson.org>
2011-01-27 17:14:41 +0100

    Also test 0 default values.

--- liboath/tests/tst_totp_algo.c
+++ liboath/tests/tst_totp_algo.c
@@ -95,6 +95,26 @@ main (void)
 	  else
 	    return 1;
 	}
+
+      rc = oath_totp_generate (secret, secretlen, tv[i].secs,
+			       0, 0, 8, otp);
+      if (rc != OATH_OK)
+	{
+	  printf ("oath_totp_generate_time2: %d\n", rc);
+	  return 1;
+	}
+
+
+      if (strcmp (otp, tv[i].otp) != 0)
+	{
+	  printf ("otp[%d] got %s expected2 %s\n",
+		  i, otp, tv[i].otp);
+	  if (strcmp (otp, "82762030") == 0
+	      && strcmp (tv[i].otp, "65353130") == 0)
+	    printf ("Mismatch due to 32-bit time_t...\n");
+	  else
+	    return 1;
+	}
     }
 
   rc = oath_done ();
