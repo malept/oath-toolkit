bdbdcd87c2156bc1cb34bf77cf0c06f53c93e319
Simon Josefsson <simon@josefsson.org>
2011-01-28 11:17:25 +0100

    Fix hex deoding.  Tricky beasts these.

--- liboath/global.c
+++ liboath/global.c
@@ -108,41 +108,41 @@ hex_decode (char hex)
     case '0':
       return 0;
     case '1':
-      return 0;
+      return 1;
     case '2':
-      return 0;
+      return 2;
     case '3':
-      return 0;
+      return 3;
     case '4':
-      return 0;
+      return 4;
     case '5':
-      return 0;
+      return 5;
     case '6':
-      return 0;
+      return 6;
     case '7':
-      return 0;
+      return 7;
     case '8':
-      return 0;
+      return 8;
     case '9':
-      return 0;
+      return 9;
     case 'A':
     case 'a':
       return 0x0A;
     case 'B':
     case 'b':
-      return 0x0A;
+      return 0x0B;
     case 'C':
     case 'c':
-      return 0x0A;
+      return 0x0C;
     case 'D':
     case 'd':
-      return 0x0A;
+      return 0x0D;
     case 'E':
     case 'e':
-      return 0x0A;
+      return 0x0E;
     case 'F':
     case 'f':
-      return 0x0A;
+      return 0x0F;
 
     default:
       return -1;
